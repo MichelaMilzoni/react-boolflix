4328ed486fb8d216866a11cf911d5b86e3e04bef
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-06-06 11:48:33 +0200

    modificato l'endpoint di ricerca esistente per film e serie tv aggiornando la logica di map dei risultati all'interno della route per includere i nomi dei generi

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -84,7 +84,19 @@ router.get('/', async (req, res) => {
             poster_path: movie.poster_path,
             overview: movie.overview,
             release_date: movie.release_date, // Per coerenza con le serie TV
-            media_type: 'movie' // Aggiungiamo un campo per distinguere il tipo
+            media_type: 'movie', // Aggiungiamo un campo per distinguere il tipo
+            genres: movie.genre_ids 
+                ? 
+                movie.genre_ids.map(id => 
+                movieGenresMap.get(id))
+                .filter(Boolean) // Rimuove i generi vuoti
+                : []
+                //! ? : ternario (versione compatta if..else) per gestire i casi in cui non ci sono generi
+                //! CONDIZIONE: movie.genre_ids se esiste e non è NULL o undefined esegui il primo blocco (? ...)
+                //! SE NO: esegui il secondo blocco (: [])
+                //! .map applica una funzione a ogni elemento e restituisce un nuovo array
+                //! .filter rimuove gli elementi che non soddisfano la condizione (in questo caso, rimuove i generi vuoti)
+                //! Boolean (oppure name => name) ripulisce l'array rimuovendo elementi false
         }));
 
         //* Standardizzo i risultati delle serie TV
@@ -97,7 +109,14 @@ router.get('/', async (req, res) => {
             poster_path: tvShow.poster_path,
             overview: tvShow.overview,
             release_date: tvShow.first_air_date, // Le serie TV hanno 'first_air_date'
-            media_type: 'tv' // Aggiungo un campo per distinguere il tipo
+            media_type: 'tv', // Aggiungo un campo per distinguere il tipo
+            genres: tvShow.genre_ids 
+                ? 
+                tvShow.genre_ids.map(id => 
+                tvGenresMap.get(id))
+                .filter(Boolean) 
+                : []
+                //! vedi sopra per la spiegazione del ternario e del filter
         }));
 
         //* Combina tutti i risultati in un unico array
